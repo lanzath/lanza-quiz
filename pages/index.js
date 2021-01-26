cb6b75b908541551cb938a71841f090002030e79
Thiago Lanza <lanzathm@gmail.com>
2021-01-26 16:33:57 -0300

    Acrescentado navegação para quiz

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -18,8 +22,19 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  const browseToQuizPage = (event) => {
+    event.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  };
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>LanzaQuiz</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -27,7 +42,15 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={browseToQuizPage}>
+              <input
+                placeholder="Seu nome"
+                onChange={(event) => setName(event.target.value)}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
